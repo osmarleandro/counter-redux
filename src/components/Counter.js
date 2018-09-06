6f94c55e96d4b5e35d2788470a121e37f0d6ebc3
Osmar Leandro <osmarleandrods@gmail.com>
2018-09-06 12:08:52 -0300

    Refactoring: add new reducers

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import { increment, decrement, incrementAmount, reload, sayMyName } from "../redux/index"
+
 class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>1</h1>
+        <h1>{this.props.counter}</h1>
+        <button class="btn" onClick={() => this.props.decrement()}>-</button>
+        <button class="btn" onClick={() => this.props.increment()}>+</button>
+
+        <button class="btn" onClick={() => this.props.reload()}>reload</button>
+        <button class="btn" onClick={() => this.props.incrementAmount(10)}>+10</button>
+        
+        <h1>Hello, {this.props.name}</h1>
+        <button class="btn" onClick={() => this.props.sayMyName("Olds")}>Say My Name</button>
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  
+  counter: state.counter,
+  name: state.name
 });
 
 const mapDispatchToProps = {
-  
+  increment, decrement, incrementAmount, reload, sayMyName
 };
 
 export default connect(
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,185 +1,51 @@
-import { createStore, combineReducers, applyMiddleware, compose
-} from "redux";
-import uuidv1 from "uuid/v1";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 //actions
-export const CHANGE_ACTIVE_LIST = "CHANGE_ACTIVE_LIST";
-export const ADD_TODO = "ADD_TODO";
-export const REMOVE_TODO = "REMOVE_TODO";
-export const COMPLETE_TODO = "COMPLETE_TODO";
-export const UNCOMPLETE_TODO = "UNCOMPLETE_TODO";
-export const ADD_LIST = "ADD_LIST";
-export const DELETE_LIST = "DELETE_LIST";
-export const CLEAR_LIST = "CLEAR_LIST";
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const INCREMENT_AMOUNT = "INCREMENT_AMOUNT";
+export const RELOAD = "RELOAD";
+export const SAYMYNAME = "SAYMYNAME"
 
-export const changeActiveList = (activeList) => {
-  return { type: CHANGE_ACTIVE_LIST, activeList
-  };
-};
-
-export const addTodo = (todo, listId) => {
-  return { type: ADD_TODO, todo, listId
-  };
-};
-
-export const deleteTodo = (todo, listId) => {
-  return { type: ADD_TODO, todo, listId
-  };
+export const increment = () => {
+  return { type: INCREMENT }
 }
 
-export const removeTodo = (todoId, listId) => {
-  return { type: REMOVE_TODO, todoId, listId
-  };
+export const decrement =() => {
+  return {type : DECREMENT }
 }
-export const completeTodo = (todoId, listId) => {
-  return { type: COMPLETE_TODO, todoId, listId
-  };
+
+export const incrementAmount = (amount) => {
+  return { type: INCREMENT_AMOUNT, amount }
 }
-export const uncompleteTodo = (todoId, listId) => {
-  return { type: UNCOMPLETE_TODO, todoId, listId
-  };
+
+export const reload = () => {
+  return { type: RELOAD }
 }
-export const addList = (name) => {
-  return { type: ADD_LIST, name
-  };
+
+export const sayMyName = (name) => {
+  return { type: SAYMYNAME, name }
 }
-export const deleteList = listId => {
-  return { type: DELETE_LIST, listId
-  };
-};
-export const clearList = (listId) => {
-  return { type: CLEAR_LIST, listId
-  };
-}
-//reducers
-function lists(state = [],action) {
+
+const counter = (state = 0, action) => {
   switch (action.type) {
-    case COMPLETE_TODO:
-      return state.map(l => {
-        if (l.id === action.listId) return { ...l, todos: l.todos.map(t => {
-              if (t.id === action.todoId) return { ...t, done: true
-          };
-              return t;
-        })
-      };
-        return l;
-    });
-    case UNCOMPLETE_TODO:
-      return state.map(l => {
-        if (l.id === action.listId) return { ...l, todos: l.todos.map(t => {
-              if (t.id === action.todoId) return { ...t, done: false
-          };
-              return t;
-        })
-      };
-        return l;
-    });
-    case ADD_LIST: {
-      return state.concat([
-        { name: action.name, id: uuidv1(), todos: []
-        }
-      ]);
-    }
-    case DELETE_LIST: {
-      return state.filter(l => l.id !== action.listId);
-    }
-    case CLEAR_LIST: {
-      return state.map(l => {
-        if (l.id === action.listId) return { ...l, todos: []
-        };
-        return l;
-      });
-    }
-    case ADD_TODO:
-      return state.map(l => {
-        if (l.id === action.listId) {
-          l.todos = l.todos.concat([
-          { ...action.todo, id: uuidv1()
-          }
-        ]);
-          return l;
-      }
-        return l;
-    });
-    default:
-      return state;
+    case INCREMENT: return state + 1;
+    case DECREMENT: return state - 1;
+    case INCREMENT_AMOUNT: return state + action.amount;
+    case RELOAD: return state - state;
+    default: return state;
   }
 }
-function activeList(state = null, action) {
-  switch (action.type) {
-    case CHANGE_ACTIVE_LIST: 
-      return action.activeList;
-    case CLEAR_LIST:
-      return null
-    case DELETE_LIST:
-      return null
-    default:
-      return state;
+
+const name = (state = 'User', action) => {
+  switch(action.type){
+    case SAYMYNAME: return state + " " + action.name;
+    default: return state;
   }
 }
-function todos(state = [], action) {
-  switch (action.type) {
-    case COMPLETE_TODO:
-      return state.map(t => {
-        if (t.id === action.todoId)
-          return {
-            ...t,
-            done: true
-      }
-        return t
-    })
-    case UNCOMPLETE_TODO:
-      return state.map(t => {
-        if (t.id === action.todoId)
-          return {
-            ...t,
-            done: false
-      }
-        return t
-    })
-    case CHANGE_ACTIVE_LIST: 
-      return action.activeList.todos
-    case ADD_TODO:
-      return state.concat([
-      { ...action.todo, id: uuidv1()
-      }
-    ])
-    case CLEAR_LIST:
-      return []
-    case DELETE_LIST:
-      return []
-    default:
-      return state;
-  }
-}
-function completed(state = 0, action) {
-  switch (action.type) {
-    case COMPLETE_TODO:
-      return state + 1;
-    case UNCOMPLETE_TODO:
-      return state - 1;
-    case ADD_TODO:
-      return action.todo.done ? state + 1 : state;
-    default:
-      return state;
-  }
-} 
-function totalOfTodos(state = 0, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return state + 1;
-    case REMOVE_TODO:
-      return state - 1;
-    default:
-      return state;
-  }
-} 
+
 const reducers = combineReducers({
-  lists,
-  activeList,
-  todos,
-  completed,
-  totalOfTodos
+  counter, name
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
